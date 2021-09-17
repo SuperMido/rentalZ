4bd3ced05b3a6f93d9bb08e25a5a2c36d260f411
Huy Tran <supermido1996@gmail.com>
2021-09-17 23:15:55 +0700

    init rentalZ project

---
+++ lib/screens/onboarding/pages/education/education_text_column.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../../widgets/text_column.dart';
+
+class EducationTextColumn extends StatelessWidget {
+  const EducationTextColumn();
+
+  @override
+  Widget build(BuildContext context) {
+    return const TextColumn(
+      title: '[1]',
+      text: '[2]',
+    );
+  }
+}
